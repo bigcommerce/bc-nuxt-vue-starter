04d6fe0306fff9a73048bd25666d2ce924f637e2
ansariswag <ameedkhalid09@gmail.com>
2021-02-08 14:23:26 -0800

    update login view

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -6,46 +6,50 @@
     </div>
     <div v-else class="login-row">
       <h1 class="page-header">Log In</h1>
-      <div class="login-form">
-        <SfInput
-          ref="email"
-          v-model="email"
-          :type="'text'"
-          :label="'Email'"
-          :name="'email'"
-          :valid="$v.email.required"
-          :placeholder="'Input Email Address'"
-          :error-message="'Email is required'"
-        />
-        <SfInput
-          v-model="password"
-          :type="'password'"
-          :label="'Password'"
-          :name="'password'"
-          :valid="$v.password.required"
-          :placeholder="'Input Password'"
-          :error-message="'Password is required'"
-        />
-        <SfButton :disabled="btnDisabled" @click="handleLogin">Log In</SfButton>
-      </div>
-      <div class="new-customer-form">
-        <div class="new-customer">
-          <div class="panel">
-            <div class="panel-header">
-              <h2 class="panel-title">New Customer?</h2>
-            </div>
-            <div class="panel-body">
-              <p class="new-customer-intro">
-                Create an account with us and you'll be able to:
-              </p>
-              <ul class="new-customer-fact-list">
-                <li>Check out faster</li>
-                <li>Save multiple shipping addresses</li>
-                <li>Access your order history</li>
-                <li>Track new orders</li>
-                <li>Save items to your Wish List</li>
-              </ul>
-              <SfButton link="/register">Create</SfButton>
+      <div class="customer-form">
+        <div class="login-form">
+          <SfInput
+            ref="email"
+            v-model="email"
+            :type="'text'"
+            :label="'Email'"
+            :name="'email'"
+            :valid="$v.email.required"
+            :placeholder="'Input Email Address'"
+            :error-message="'Email is required'"
+          />
+          <SfInput
+            v-model="password"
+            :type="'password'"
+            :label="'Password'"
+            :name="'password'"
+            :valid="$v.password.required"
+            :placeholder="'Input Password'"
+            :error-message="'Password is required'"
+          />
+          <SfButton :disabled="btnDisabled" @click="handleLogin"
+            >Log In</SfButton
+          >
+        </div>
+        <div class="new-customer-form">
+          <div class="new-customer">
+            <div class="panel">
+              <div class="panel-header">
+                <h2 class="panel-title">New Customer?</h2>
+              </div>
+              <div class="panel-body">
+                <p class="new-customer-intro">
+                  Create an account with us and you'll be able to:
+                </p>
+                <ul class="new-customer-fact-list">
+                  <li>Check out faster</li>
+                  <li>Save multiple shipping addresses</li>
+                  <li>Access your order history</li>
+                  <li>Track new orders</li>
+                  <li>Save items to your Wish List</li>
+                </ul>
+                <SfButton link="/register">Create</SfButton>
+              </div>
             </div>
           </div>
         </div>
