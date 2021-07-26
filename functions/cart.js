9939cf835313e13dd66e49caca0b2794691c628a
ansariswag <ameedkhalid09@gmail.com>
2021-07-25 18:28:25 -0700

    functions

---
+++ functions/cart.js
@@ -0,0 +1,14 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const handler = async (event, context) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World';
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` })
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
+
+module.exports = { handler };
